81dbb7d6db057567b77ae9326688db372d29a6ea
Meher Sahitya Prasad Peketi <139023269+Mehermsp@users.noreply.github.com>
2025-09-06 18:59:38 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-export const API_BASE =
-    import.meta.env.VITE_API_BASE || "https://cache2k25-backend.onrender.com";
+export const API_BASE ="https://cache2k25-backend.onrender.com";
 
 export const api = axios.create({ baseURL: `${API_BASE}/api` });
 
